98136e2a3ed64717d7f036149a44890c8c8ae66b
Adam Pawełczak <adam.pawelczak@nngsolutions.com>
2022-12-11 20:58:15 +0100

    Szkielet z rendererem i ustawieniami do testów na danych rzeczywistych

---
+++ block_categorytiles.php
@@ -0,0 +1,99 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Block categorytiles is defined here.
+ *
+ * @package     block_categorytiles
+ * @copyright   2022 Adam Pawełczak <adam.pawelczak@nngsolutions.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_categorytiles extends block_base {
+
+    /**
+     * Initializes class member variables.
+     */
+    public function init() {
+        // Needed by Moodle to differentiate between blocks.
+        $this->title = get_string('pluginname', 'block_categorytiles');
+    }
+
+    /**
+     * Returns the block contents.
+     *
+     * @return stdClass The block contents.
+     */
+    public function get_content() {
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        if (empty($this->instance)) {
+            $this->content = '';
+            return $this->content;
+        }
+
+        $renderable = new \block_categorytiles\output\main();
+        $renderer = $this->page->get_renderer('block_categorytiles');
+
+        $this->content = new stdClass();
+        $this->content->text = $renderer->render($renderable);
+        $this->content->footer = '';
+
+        if (!empty($this->config->text)) {
+            $this->content->text = $this->config->text;
+        } else {
+            $text = 'Please define the content text in /blocks/categorytiles/block_categorytiles.php.';
+            $this->content->text = $text;
+        }
+
+        return $this->content;
+    }
+
+    /**
+     * Defines configuration data.
+     *
+     * The function is called immediately after init().
+     */
+    public function specialization() {
+
+        // Load user defined title and make sure it's never empty.
+        if (empty($this->config->title)) {
+            $this->title = get_string('pluginname', 'block_categorytiles');
+        } else {
+            $this->title = $this->config->title;
+        }
+    }
+
+    /**
+     * Enables global configuration of the block in settings.php.
+     *
+     * @return bool True if the global configuration is enabled.
+     */
+    public function has_config() {
+        return true;
+    }
+
+    /**
+     * Sets the applicable formats for the block.
+     *
+     * @return string[] Array of pages and permissions.
+     */
+    public function applicable_formats() {
+        return array( 'site' => true, 'my' => true );
+    }
+}
